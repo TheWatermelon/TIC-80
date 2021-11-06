895aeb6f511239f6df2ccfb543a09c82629b1e35
TheWatermelon <unconfigured@null.spigotmc.org>
2021-11-06 19:08:31 +0100

    snake: fixed insta death when pressing multiple buttons at once, better graphics for scores, pause in-game

--- style.sh
+++ style.sh
@@ -4,12 +4,9 @@ FILENAME=$1
 TOTAL_LINES=$(wc -l $FILENAME | awk '{print $1}')
 END_HEADER=$(grep -n '</head>' $FILENAME | awk -F ':' '{print $1}')
 LINE_NUMBER=$[ $END_HEADER - 1 ]
-LINE_TO_ADD='<link rel="stylesheet" src="stylesheet.css">'
+LINE_TO_ADD='<link rel="stylesheet" href="stylesheet.css">'
 REMAINING_LINES=$[ $TOTAL_LINES - $LINE_NUMBER ]
 
-CMD='sed -i '"'"$LINE_NUMBER'i<link rel="stylesheet" href="stylesheet.css" />'"' "$FILENAME
-
-#$CMD
 head -n $LINE_NUMBER $FILENAME > .tmp
 echo $LINE_TO_ADD >> .tmp
 tail -n $REMAINING_LINES $FILENAME >> .tmp
